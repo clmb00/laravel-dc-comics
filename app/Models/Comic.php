4ab9543a0cc6ac821d85f9e6346a00c0653c817a
clmb00 <paolo.colombo2000@gmail.com>
2023-01-12 15:26:16 +0100

    [add] migration and seeder

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Comic extends Model
 {
     use HasFactory;
+
+    public static function create_slug($string){
+
+        $slug = Str::slug($string, '-');
+        $original_slug = $slug;
+
+        $c=0;
+        // questo funziona
+        $comic_exist = Comic::where('slug', $slug)->first();
+        while ($comic_exist) {
+            $c++;
+            $slug = $original_slug . $c;
+            $comic_exist = Comic::where('slug', $slug)->first();
+        }
+
+        return $slug;
+    }
 }
--- database/migrations/2023_01_12_140246_create_comics_table.php
+++ database/migrations/2023_01_12_140246_create_comics_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 60);
+            $table->string('slug', 70);
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price');
+            $table->string('series', 60);
+            $table->date('sale_date');
+            $table->string('type', 40);
             $table->timestamps();
         });
     }
