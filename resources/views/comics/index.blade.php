6e79209c8c321ce8ad5a91ad9f3823cd2f320940
clmb00 <paolo.colombo2000@gmail.com>
2023-01-12 15:46:35 +0100

    [add] table with comics

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <h1>Comics</h1>
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Price</th>
+                <th scope="col">Series</th>
+                <th scope="col">Sale Date</th>
+                <th scope="col">Type</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+                @forelse ($comics as $comic)
+                    <tr>
+                        <td>{{ $comic->id }}</td>
+                        <td>{{ $comic->title }}</td>
+                        <td>{{ $comic->description }}</td>
+                        <td>{{ $comic->price }}</td>
+                        <td>{{ $comic->series }}</td>
+                        <td>{{ $comic->sale_date }}</td>
+                        <td>{{ $comic->type }}</td>
+                        <td>---</td>
+                    </tr>
+                @empty
+                    <h2>No result.</h2>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+@endsection
